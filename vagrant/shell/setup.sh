c312233536c573560a6d907764ef2f0bd1e9299a
Thomas Taschner <ttaschner@student.tgm.ac.at>
2016-04-21 16:11:26 +0200

    Updated setup for vagrant vox

--- vagrant/shell/setup.sh
+++ vagrant/shell/setup.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
 
-VERSION=0.8.1
-EXT=tar.xz
-
 apt-get update
 apt-get upgrade -y
 
-apt-get install scons build-essential pkg-config -y
-apt-get install libboost-dev uuid-dev libfuse-dev libevent-dev libssl-dev -y
+apt-get install ori -y
 
 apt-get dist-upgrade -y
 apt-get autoremove -y
 apt-get clean
-
-wget https://bitbucket.org/orifs/ori/downloads/ori-$VERSION.$EXT
-tar xvfJ ori-$VERSION.$EXT
-cd ori-$VERSION
-scons
-
-scons install
